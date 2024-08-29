08b4aae1592dee181ce0822eb0ea5c9067de538e
Alex Kyuchukov <alexkyuchukov@gmail.com>
2024-08-28 20:26:41 -0700

    Added solar widget. Change zone number labels. Adjusted website features slightly

--- analyzeData.py
+++ analyzeData.py
@@ -28,6 +28,19 @@ sparse = args.lower
 busy = args.upper
 span = args.range
 
+# keeping this in case I have to name zones by callsign
+zone_name_map = {
+    1: 'NA-1', 2: 'NA-2', 3: 'NA-3', 4: 'NA-4', 5: 'NA-5',
+    6: 'NA-6', 7: 'NA-7', 8: 'NA-8', 9: 'SA-9', 10: 'SA-10',
+    11: 'SA-11', 12: 'SA-12', 13: 'EU-13', 14: 'EU-14',
+    15: 'EU-15', 16: 'EU-16', 17: 'EU-17', 18: 'EU-18',
+    19: 'AS-19', 20: 'EU/AS-20', 21: 'AS-21', 22: 'AS-22',
+    23: 'AS-23', 24: 'AS-24', 25: 'AS-25', 26: 'AS-26',
+    27: 'AS-27', 28: 'AS-28', 29: 'OC-29', 30: 'OC-30',
+    31: 'OC-31', 32: 'OC-32', 33: 'AF-33', 34: 'AF-34',
+    35: 'AF-35', 36: 'AF-36', 37: 'AF-37', 38: 'AF-38',
+    39: 'AF-39', 40: 'EU-40'
+}
 
 def get_aws_credentials():
     """
@@ -38,6 +51,7 @@ def get_aws_credentials():
     access_key = input("Enter your AWS Access Key ID: ")
     secret_key = input("Enter your AWS Secret Access Key: ")
     bucket = input("Enter the name of the S3 Bucket you'd like to write to: ")
+
     return {
         'aws_access_key_id': access_key,
         'aws_secret_access_key': secret_key,
@@ -71,10 +85,8 @@ def upload_file_to_s3(file_name, bucket_name, acc_key, sec_key):
 
 def reformat_table(table):
     flattened = pd.DataFrame(table.to_records())
-    # flattened['Zone'] = flattened['Zone'].apply(lambda x: f"{x} {zone_name_map.get(x, '')}")
+    flattened['Zone'] = flattened['Zone'].apply(lambda x: zone_name_map.get(x, ''))
     flattened.reset_index(drop=True)
-    # flattened['Zone '] = flattened['Zone']
-    # flattened['Zone '] = flattened.loc[:, 'Zone']
     flattened1 = (flattened.reindex(
         ['Zone', '160', '80', '40', '20', '15', '10', '6', ' ', '30', '17', '12'], axis=1))
 
@@ -152,80 +164,91 @@ def run(access_key, secret_key, s3_buck):
 
     # Apply larger font sizes to the first column and header row
     styled_table1.set_table_styles([
-        {'selector': 'caption', 'props': [('font-size', '12pt'), ('font-weight', 'bold')]},
+        {'selector': 'caption', 'props': [('font-size', '13pt'), ('font-weight', 'bold')]},
         {'selector': 'th',
-         'props': [('font-size', '12pt'), ('padding', '10px'), ('word-wrap', 'break-word'), ('position', 'sticky'),
+         'props': [('font-size', '12pt'), ('word-wrap', 'break-word'), ('position', 'sticky'),
                    ('top', '0'), ('background-color', 'rgba(255, 255, 255, 0.75)'), ('z-index', '1')]},
-        {'selector': 'td:first-child', 'props': [('font-size', '12pt'), ('padding', '10px')]},  # First column
-        {'selector': 'td', 'props': [('font-size', '10pt'), ('padding', '8px'), ('word-wrap', 'break-word')]},
+        {'selector': 'td:first-child', 'props': [('font-size', '12pt')]},  # First column
+        {'selector': 'td',
+         'props': [('font-size', '10pt'), ('padding-left', 'calc(5px + 1vw)'), ('padding-top', '4px'), ('padding-bottom', '4px'), ('padding-right', 'calc(5px + 1vw)')]}
     ])
 
     # Convert the styled table to HTML
     html1 = styled_table1.hide(axis="index").to_html()
 
-    # Add specific widths for columns
-    # Modify the table HTML to include colgroup for column widths
     html1 = html1.replace('<table ',
-                          '<table style="width: 40%; table-layout: fixed; margin-left: auto; margin-right: auto;" ')
+                          '<table style="width: 60vw; table-layout: fixed; margin-left: auto; margin-right: auto;" ')
     html1 = html1.replace('<thead>',
-                          '<thead><colgroup><col style="width: 55px;"></colgroup>')
+                          '<thead><colgroup><col style="width: 117px;"></colgroup>')
 
     # legend HTML
     legend_html = f"""
      <head> <meta http-equiv="refresh" content="60"> </head> 
-     <div style="position: fixed; bottom: 0; left: 50%; transform: translateX(-50%); width: 80%; background-color: rgba(255, 255, 255, 0.75); padding: 10px; border: 1px solid gray; box-sizing: border-box; z-index: 1000;">
+     <div style="position: fixed; bottom: 0; left: 50%; transform: translateX(-50%); width: 90%; background-color: rgba(255, 255, 255, 0.75); padding: 10px; border: 1px solid gray; box-sizing: border-box; z-index: 1000;">
          <div style="display: flex; justify-content: space-around; margin-bottom: 10px;">
-             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 10pt;">
+             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 12pt;">
                  <div style="width: 20px; height: 20px; background-color: #a3cce9; margin-right: 5px;"></div>
                  <div>Marginal (≤ -15 dB)</div>
              </div>
-             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 10pt;">
+             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 12pt;">
                  <div style="width: 20px; height: 20px; background-color: #b6e3b5; margin-right: 5px;"></div>
                  <div>Normal (-15 to -10 dB)</div>
              </div>
-             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 10pt;">
+             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 12pt;">
                  <div style="width: 20px; height: 20px; background-color: #f7c896; margin-right: 5px;"></div>
                  <div>Above Average (-10 to -3 dB)</div>
              </div>
-             <div style="display: flex; align-items: center; font-size: 10pt;">
+             <div style="display: flex; align-items: center; font-size: 12pt;">
                  <div style="width: 20px; height: 20px; background-color: #e57373; margin-right: 5px;"></div>
                  <div>Hot (> -3 dB)</div>
              </div>
          </div>
          <div style="display: flex; justify-content: space-around;">
-             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 10pt;">
+             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 12pt;">
                  <div style="font-size: 20px; margin-right: 5px;">◻</div>
                  <div>Quiet (≤ {sparse} spots)</div>
              </div>
-             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 10pt;">
+             <div style="display: flex; align-items: center; margin-right: 20px; font-size: 12pt;">
                  <div style="font-size: 20px; margin-right: 5px;">◩</div>
                  <div>Moderate ({sparse + 1} to {busy - 1} spots)</div>
              </div>
-             <div style="display: flex; align-items: center; font-size: 10pt;">
+             <div style="display: flex; align-items: center; font-size: 12pt;">
                  <div style="font-size: 20px; margin-right: 5px;">◼</div>
                  <div>Busy (≥ {busy} spots)</div>
              </div>
          </div>
      </div>
      """
+    
+    # HTML for the solar widget
+    solar_widget_html = """
+    <div style="position: fixed; left: 0; top: 0; padding: 10px; z-index: 1000;">
+        <a href="https://www.hamqsl.com/solar.html" title="Click to add Solar-Terrestrial Data to your website!">
+            <img src="https://www.hamqsl.com/solar100sc.php">
+        </a>
+    </div>
+    """
 
-    # final HTML with scrollable table
+    # final HTML with scrollable table, solar widget, and no vertical scrollbar
     final_html = f"""
-     <div style="position: relative; width: 100%; height: 100%;">
-         <div style="max-height: 80vh; overflow-y: auto; padding-bottom: 10%;"> <!-- This div creates the scrollable area -->
-             {html1}
-         </div>
-         <div>{legend_html}</div>
-     </div>
-     """
-
-    # final HTML with scrollable table
-    final_html = f"""
-    <div style="position: relative; width: 100%; height: 100%;">
-        <div style="max-height: 80vh; overflow-y: auto; padding-bottom: 10%;"> <!-- This div creates the scrollable area -->
-            {html1}
+    <style>
+        body {{
+            margin: 0;
+            padding: 0;
+            overflow-y: hidden; /* Prevent vertical scrolling */
+        }}
+        html {{
+            height: 100%;
+        }}
+    </style>
+    <div style="display: flex; width: 100%; height: 100%;">
+        {solar_widget_html}
+        <div style="position: relative; flex-grow: 1; padding-left: 100px; overflow-y: auto;"> <!-- Added padding to accommodate the widget -->
+            <div style="max-height: 80vh; overflow-y: auto; padding-bottom: 10%;"> <!-- This div creates the scrollable area -->
+                {html1}
+            </div>
+            <div>{legend_html}</div>
         </div>
-        <div>{legend_html}</div>
     </div>
     """
 
